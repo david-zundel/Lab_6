e49e10917b0eab53756dfa970d3e00f863227884
David Zundel <davidzundel11@gmail.com>
2024-03-17 22:47:14 -0400

    David Zundel: added encode() function and main function to main.py file

---
+++ main.py
@@ -0,0 +1,24 @@
+# David Zundel
+def encode(password1):
+    new_password = ""
+    for char in password1:
+        new_password += str(int(char)+3)
+    return new_password
+
+
+password = ""
+cont = True
+while cont:
+    print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n")
+    option = int(input("Please enter an option: "))
+    if option == 1:
+        temp = input("Please enter your password to encode: ")
+        password = encode(temp)
+        print("Your password has been encoded and stored!\n")
+    elif option == 2:
+        pass
+        # decoded_pass = decode(password)
+        # print(f"The encoded password is {password}, and the original password is {decoded_pass}.\n")
+    else:
+        break
+
